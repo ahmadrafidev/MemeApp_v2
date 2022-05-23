cf2ada1b115a3c82cc08c2296f9b250985a5f292
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2022-05-23 21:20:04 +0700

    Initial Commit

---
+++ MemeApp_v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemeApp_v2
+//
+//  Created by Ahmad Rafi Wirana on 23/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemeApp_v2/MemeApp_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  MemeApp_v2App.swift
+//  MemeApp_v2
+//
+//  Created by Ahmad Rafi Wirana on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MemeApp_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
